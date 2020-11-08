143d474362a322185e771cd81e188124198e530a
Louby <loubyc2@gmail.com>
2020-11-07 22:50:10 -0300

    some change

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,24 +7,26 @@ import './Header.css';
 function Header() {
     return (
         <div className='header'>
-            <div className="header__banner">
-                <div className="header__banner-main">
-                    <div className='header__banner-text'>
+            <div className="header__banner d-flex justify-content-center pt-4">
+                <div className="header__banner-main container row">
+                    <div className='header__banner-text col'>
                         <h4>Hola! Soy <br />  Louby Claméus</h4>
-                        <hr className='hr-top' />
+                        <div className='hr my-3'></div>
 
                         <p>
                             Soy un <strong>front-end</strong> web devlopper.
                             Me dedico a crear moderna pagina web y, soy diseñador grafico tambien.
                         </p>
                         <Link to='/'>
-
+                            <button className='btn text-uppercase btn-outline-success'>mandarme un email</button>
                         </Link>
                     </div>
-                    <img className='header__banner-img' src={portfolio} alt="portfolio img" />
+                    <div className="img col ">
+                        <img className='header__banner-img ' src={portfolio} alt="portfolio img" />
+                    </div>
                 </div>
             </div>
-            <div className='header__banner-bottom'>
+            <div className='header__banner-bottom d-flex justify-content-center'>
                 <nav className='header__nav'>
                     <hr />
                     <ul className='header__nav-links'>
